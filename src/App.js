4ad586b1279b4228c2c2253657b2a944afe03fd7
Radmir <radmir_96@mail.ru>
2022-08-26 18:01:42 +0500

    Swiper slider add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Banner from './components/Banner/Banner'
+import Testimonials from './components/Testimonials/Testimonials'
 
 const App = () => {
   return (
     <div>
       <Banner />
+      <Testimonials />
       <div className='h-[2000px]'></div>
     </div>
   )
---
+++ src/components/Testimonials/Testimonials.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import TestimonialSlider from './TestimonialSlider/TestimonialSlider'
+
+const Testimonials = () => {
+    return (
+        <section className='h-[364px] lg:mb-[120px]'>
+            <div className='container mx-auto h-full flex items-center justify-end'>
+                <div className='w-full mx-auto lg:mx-0 lg:max-w-[570px]'>
+                    <TestimonialSlider />
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Testimonials
