393ec1be9f28ffc17bb254cab03f69b3ca1b1239
Radmir <radmir_96@mail.ru>
2022-08-26 18:15:23 +0500

    block skills added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Banner from './components/Banner/Banner'
+import Skills from './components/Skills/Skills'
 import Testimonials from './components/Testimonials/Testimonials'
 
 const App = () => {
@@ -7,6 +8,7 @@ const App = () => {
     <div>
       <Banner />
       <Testimonials />
+      <Skills />
       <div className='h-[2000px]'></div>
     </div>
   )
